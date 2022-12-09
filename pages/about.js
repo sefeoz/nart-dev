04e78091afe43c09090c0bf52a0e7a80a42fc0e6
Bahanart1 <69577746+Bahanart1@users.noreply.github.com>
2022-12-09 18:44:54 +0300

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,10 +2,10 @@ function About(){
     return(<>
         <section className="site-6xl-container mt-16">
             <h1 className="text-white text-4xl font-extrabold text-center">Hakkımızda</h1>
-            <p className="text-gray-200 text-center text-3xl font-thin mt-5">Nart Yazılım Olarak Gelecek İçin Çalışmaktan Gurur Duyuyoruz.</p>
+            <p className="text-gray-200 text-center text-3xl font-thin mt-5">Nart Developer Olarak Gelecek İçin Çalışmaktan Gurur Duyuyoruz.</p>
             <div className={"bg-white p-10 my-10 rounded-xl"}>
-                <p className={"text-3xl font-extrabold text-gray-800"}>Nart Yazılım, 2022 yılında Nart tarafından kurulmuş bağımsız bir yazılım şirketidir.</p>
-                <p className={"text-xl mt-5 font-light text-gray-700"}>Nart Yazılım'ın amacı geleceği herkese hazır hale getirebilmek için dünyadaki herkesin yazılım ile bilgilendirilmesi ve herkese bu gelecekte birer yer verilmesidir. Bunun için herkesi yazılım gibi çeşitli teknolojik alanlarda onlara bir olanak ve yardımcı bir alan oluşturmaktadır.</p>
+                <p className={"text-3xl font-extrabold text-gray-800"}>Nart Developer, 2022 yılında Nart tarafından kurulmuş bağımsız bir yazılım şirketidir.</p>
+                <p className={"text-xl mt-5 font-light text-gray-700"}>Nart Developer'ın amacı geleceği herkese hazır hale getirebilmek için dünyadaki herkesin yazılım ile bilgilendirilmesi ve herkese bu gelecekte birer yer verilmesidir. Bunun için herkesi yazılım gibi çeşitli teknolojik alanlarda onlara bir olanak ve yardımcı bir alan oluşturmaktadır.</p>
             </div>
         </section>
             {/*<----------------------------SECTION END------------------------------------------------------>*/}
@@ -14,11 +14,11 @@ function About(){
                 <p className="text-white font-thin text-center text-3xl mt-5">Kurulduğumuz andan beri gelişmekte olup bu gelişmeleri sizlerle paylaşıyoruz.</p>
                 <div className="lg:grid grid-cols-3 mt-10">
                     <div className="mt-10">
-                        <h1 className={"text-nav-green text-8xl text-center"}>28M</h1>
+                        <h1 className={"text-nav-green text-8xl text-center"}>28K</h1>
                         <p className="text-white text-3xl text-center mt-10 font-bold">Kod Satırı</p>
                     </div>
                     <div className={"mt-10"}>
-                        <h1 className={"text-nav-green text-8xl text-center"}>5</h1>
+                        <h1 className={"text-nav-green text-8xl text-center"}>6</h1>
                         <p className="text-white text-3xl text-center mt-10 font-bold">Ekibimiz</p>
                     </div>
                     <div className="mt-10">
