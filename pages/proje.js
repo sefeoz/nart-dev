e27999382ef22a96504337c8ca2f408a674b275e
Bahanart1 <69577746+Bahanart1@users.noreply.github.com>
2023-01-21 16:05:16 +0300

    Update proje.js
    yeni projeler eklendi

--- pages/proje.js
+++ pages/proje.js
@@ -21,6 +21,54 @@ function Proje(){
 
                     </div>
                 </div>
+                <div className="site-6xl-container">
+                    <div className="lg:grid grid-cols-3">
+                        <div className="mx-5 my-5">
+                            <img className={"mx-auto rounded-xl"} src="https://picsum.photos/id/60/300/300/" alt=""/>
+                            <p className={"text-center text-2xl font-bold mt-5"}>C ile Hesap Makinesi</p>
+                        <Link href={"/hesap-sistemi"}>
+                    <button className="text-center bg-white hover:bg-nav-green transition py-2 px-5 rounded-full text-center">Devam Et <FontAwesomeIcon className="my-auto" icon={faArrowRight}/></button>
+                        </Link>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="site-6xl-container">
+                    <div className="lg:grid grid-cols-3">
+                        <div className="mx-5 my-5">
+                            <img className={"mx-auto rounded-xl"} src="https://picsum.photos/id/60/300/300/" alt=""/>
+                            <p className={"text-center text-2xl font-bold mt-5"}>C ile Basit ATM</p>
+                        <Link href={"/atm-sistemi"}>
+                    <button className="text-center bg-white hover:bg-nav-green transition py-2 px-5 rounded-full text-center">Devam Et <FontAwesomeIcon className="my-auto" icon={faArrowRight}/></button>
+                        </Link>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="site-6xl-container">
+                    <div className="lg:grid grid-cols-3">
+                        <div className="mx-5 my-5">
+                            <img className={"mx-auto rounded-xl"} src="https://picsum.photos/id/60/300/300/" alt=""/>
+                            <p className={"text-center text-2xl font-bold mt-5"}>C ile Sayıları Büyükten Küçüğe Sıralama</p>
+                        <Link href={"/buyuk-sistemi"}>
+                    <button className="text-center bg-white hover:bg-nav-green transition py-2 px-5 rounded-full text-center">Devam Et <FontAwesomeIcon className="my-auto" icon={faArrowRight}/></button>
+                        </Link>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="site-6xl-container">
+                    <div className="lg:grid grid-cols-3">
+                        <div className="mx-5 my-5">
+                            <img className={"mx-auto rounded-xl"} src="https://picsum.photos/id/60/300/300/" alt=""/>
+                            <p className={"text-center text-2xl font-bold mt-5"}>C ile Not Hesaplama Sistemi</p>
+                        <Link href={"/not-sistemi"}>
+                    <button className="text-center bg-white hover:bg-nav-green transition py-2 px-5 rounded-full text-center">Devam Et <FontAwesomeIcon className="my-auto" icon={faArrowRight}/></button>
+                        </Link>
+                        </div>
+
+                    </div>
+                </div>
             </section>
         </>
     )
