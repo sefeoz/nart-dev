e429a4d51c3d11fbbc483c58f1f3348bac1fc655
Bahanart1 <69577746+Bahanart1@users.noreply.github.com>
2025-02-15 01:38:41 +0300

    .

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,8 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 export default function About() {
+  const imageUrl = "https://www.atlas.edu.tr/wp-content/uploads/2024/02/software-engineering.jpg";
+
   return (
     <main className="min-h-screen bg-gradient-to-r from-gray-800 to-gray-900 flex flex-col">
       <Navbar />
@@ -67,6 +69,56 @@ export default function About() {
                   <h3 className="text-xl font-semibold text-black">Yenilikçilik</h3>
                   <p className="mt-2 text-black">Sürekli gelişen teknoloji ve trendleri takip ediyoruz.</p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="py-10">
+        <div className="max-w-6xl mx-auto">
+          <h2 className="text-3xl font-semibold text-white text-center mb-4">Takımımız</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+            <div className="relative h-64 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
+              <img src="https://www.atlas.edu.tr/wp-content/uploads/2024/02/software-engineering.jpg" alt="Ali Yılmaz" className="w-full h-32 object-cover rounded-t-md" />
+              <div className="bg-white bg-opacity-40 backdrop-blur-md rounded-b-md flex items-center justify-center h-32">
+                <h3 className="text-xl font-semibold text-black">Baha Nart Şenel</h3>
+                <p className="mt-2 text-black">Founder</p>
+              </div>
+            </div>
+            <div className="relative h-64 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
+              <img src="https://www.atlas.edu.tr/wp-content/uploads/2024/02/software-engineering.jpg" alt="Ayşe Demir" className="w-full h-32 object-cover rounded-t-md" />
+              <div className="bg-white bg-opacity-40 backdrop-blur-md rounded-b-md flex items-center justify-center h-32">
+                <h3 className="text-xl font-semibold text-black">Şadan Efe Öz</h3>
+                <p className="mt-2 text-black">Takım Lideri</p>
+              </div>
+            </div>
+            <div className="relative h-64 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
+              <img src="https://www.atlas.edu.tr/wp-content/uploads/2024/02/software-engineering.jpg" alt="Mehmet Öz" className="w-full h-32 object-cover rounded-t-md" />
+              <div className="bg-white bg-opacity-40 backdrop-blur-md rounded-b-md flex items-center justify-center h-32">
+                <h3 className="text-xl font-semibold text-black">Emirhan Şen</h3>
+                <p className="mt-2 text-black">Tasarımcı</p>
+              </div>
+            </div>
+            <div className="relative h-64 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
+              <img src="https://www.atlas.edu.tr/wp-content/uploads/2024/02/software-engineering.jpg" alt="Fatma Korkmaz" className="w-full h-32 object-cover rounded-t-md" />
+              <div className="bg-white bg-opacity-40 backdrop-blur-md rounded-b-md flex items-center justify-center h-32">
+                <h3 className="text-xl font-semibold text-black">Oğuzhan Kavuncuoğlu</h3>
+                <p className="mt-2 text-black">Pazarlama Uzmanı</p>
+              </div>
+            </div>
+            <div className="relative h-64 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
+              <img src="https://www.atlas.edu.tr/wp-content/uploads/2024/02/software-engineering.jpg" alt="Emre Çelik" className="w-full h-32 object-cover rounded-t-md" />
+              <div className="bg-white bg-opacity-40 backdrop-blur-md rounded-b-md flex items-center justify-center h-32">
+                <h3 className="text-xl font-semibold text-black">Bora Bedirhan Sarıhan</h3>
+                <p className="mt-2 text-black">Yazılım Geliştirme</p>
+              </div>
+            </div>
+            <div className="relative h-64 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
+              <img src="https://www.atlas.edu.tr/wp-content/uploads/2024/02/software-engineering.jpg" alt="Zeynep Arslan" className="w-full h-32 object-cover rounded-t-md" />
+              <div className="bg-white bg-opacity-40 backdrop-blur-md rounded-b-md flex items-center justify-center h-32">
+                <h3 className="text-xl font-semibold text-black">Ahmet Bedirhan Pilatin</h3>
+                <p className="mt-2 text-black">Muhasebeci</p>
               </div>
             </div>
           </div>
